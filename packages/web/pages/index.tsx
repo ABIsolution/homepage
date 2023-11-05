96d36c72246e5abf8c5028e1bba5650ea384f354
Abismall <abismayn@gmail.com>
2023-11-05 18:38:52 +0200

    use Typography

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,13 +1,10 @@
-import { Inter } from 'next/font/google';
+import { Typography } from '../components/common';
 import Layout from '../components/layout';
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <Layout>
-      <div>
-        <p>Hello.</p>
-      </div>
+      <Typography variant="h1">Homepage</Typography>
     </Layout>
   );
 }
