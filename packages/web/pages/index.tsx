309e90c24ae106d98e618af84629d6b9253c31cb
Abismall <abismayn@gmail.com>
2023-11-05 12:54:50 +0200

    use layout

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,15 +1,13 @@
 import { Inter } from 'next/font/google';
-
+import Layout from '../components/layout';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
+    <Layout>
       <div>
         <p>Hello.</p>
       </div>
-    </main>
+    </Layout>
   );
 }
